f1cda6ddf786cbe354f1a0e703c1b8bb3528018a
keating <min9nim@gmail.com>
2023-12-16 23:36:55 +0900

    👔 Add or update business logic

---
+++ src/utils/index.ts
@@ -0,0 +1,33 @@
+export const isBrowser = typeof window === 'object'
+export const PROD_HOST = 'copy-n-paste.vercel.app'
+export const isProd =
+  process.env.VERCEL_GIT_COMMIT_REF === 'main' ||
+  (isBrowser && window.location.host === PROD_HOST)
+
+export const API_URL = isProd ? `https://${PROD_HOST}` : 'http://localhost:3000'
+
+export const copyToClipboard = val => {
+  let t = document.createElement('textarea')
+  document.body.appendChild(t)
+  t.value = val
+  t.select()
+  document.execCommand('copy')
+  document.body.removeChild(t)
+}
+
+export const deeplReq = (path: string, option = {}) =>
+  fetch(`https://api-free.deepl.com${path}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `DeepL-Auth-Key ${process.env.API_KEY}`,
+    },
+    ...option,
+  }).then(res => res.json())
+
+export function debounce(func, timeout = 300) {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => func(...args), timeout)
+  }
+}
