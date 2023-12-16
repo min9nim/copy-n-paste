388795694071a5d9bb029aa4789e2d3e373e0067
keating <min9nim@gmail.com>
2023-12-17 01:19:40 +0900

    update test

--- testcase/index.test.ts
+++ testcase/index.test.ts
@@ -2,16 +2,24 @@ import { MongoClient } from 'mongodb'
 
 describe('test', () => {
   test('mongodb', async () => {
-    try {
-      if (!process.env.MONGODB_URI) {
-        throw Error('MONGODB_URI is undefined')
-      }
-      const client = new MongoClient(process.env.MONGODB_URI)
+    if (!process.env.MONGODB_URI) {
+      throw Error('MONGODB_URI is undefined')
+    }
+    const client = new MongoClient(process.env.MONGODB_URI)
 
+    try {
       await client.connect()
       await listDatabases(client)
+
+      await createText(client, {
+        text: 'blabla',
+        createAt: Date.now(),
+        userId: Math.random().toString(36).slice(2),
+      })
     } catch (e) {
       console.error(e)
+    } finally {
+      await client.close()
     }
   })
 })
@@ -21,4 +29,12 @@ async function listDatabases(client) {
 
   console.log('Databases:')
   databasesList.databases.forEach(db => console.log(` - ${db.name}`))
+}
+
+async function createText(client, text) {
+  const result = await client
+    .db('copy-n-paste')
+    .collection('texts')
+    .insertOne(text)
+  console.log(`New text created with the following id: ${result.insertedId}`)
 }
