38bef1d30ff6e5b3473d1d196d2c6e1489ecfef9
keating <min9nim@gmail.com>
2023-12-17 08:51:59 +0900

    ui

--- src/app/List.tsx
+++ src/app/List.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import IconCopy from '@/components/icons/IconCopy'
 import { useEffect, useState } from 'react'
 
 export default function List() {
@@ -20,8 +21,14 @@ export default function List() {
   return (
     <div className="max-w-2xl w-full">
       {list.map(item => (
-        <div className="bg-gray-800 my-2 py-2 px-4" key={item._id}>
-          {item.text}
+        <div
+          className="flex flex-row justify-between bg-gray-800 my-2 py-2 px-4"
+          key={item._id}
+        >
+          <div>{item.text}</div>
+          <div className="hover:scale-110 cursor-pointer">
+            <IconCopy size={15} />
+          </div>
         </div>
       ))}
     </div>
