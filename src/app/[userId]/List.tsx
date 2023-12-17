7e82f95f1356cffc9c1c0278b0e117f44c2584ee
keating <min9nim@gmail.com>
2023-12-17 20:47:24 +0900

    ui

--- src/app/[userId]/List.tsx
+++ src/app/[userId]/List.tsx
@@ -11,6 +11,25 @@ export default function List({ list, loading }) {
     return <div className="animate-bounce">Loading..</div>
   }
 
+  const deleteItem = async item => {
+    const result = await Swal.fire({
+      title: `Delete this text?`,
+      html: `<span class="text-gray-400">${item.text}</span>`,
+      // icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    })
+
+    if (result.isConfirmed) {
+      await fetch('/api/delete', {
+        method: 'delete',
+        body: JSON.stringify({ _id: item._id }),
+      })
+      toast.success('deleted')
+    }
+  }
   return (
     <div className="max-w-2xl w-full">
       {list.length === 0 && (
@@ -19,9 +38,15 @@ export default function List({ list, loading }) {
         </div>
       )}
       {list.map(item => (
-        <div className="flex flex-col items-end my-2 py-2 px-4" key={item._id}>
+        <div className="flex flex-col items-end my-4 py-2 px-4" key={item._id}>
           <pre className="w-full bg-gray-800 py-2 px-4 ">{item.text}</pre>
-          <div className="flex items-center justify-end">
+          <div className="flex items-center justify-between w-full">
+            <div
+              className="hover:scale-110 cursor-pointer"
+              onClick={() => deleteItem(item)}
+            >
+              <IconDelete size={30} />
+            </div>
             <div
               className="hover:scale-110 cursor-pointer"
               onClick={() => {
@@ -30,30 +55,6 @@ export default function List({ list, loading }) {
               }}
             >
               <IconCopy size={15} />
-            </div>
-            <div
-              className="hover:scale-110 cursor-pointer"
-              onClick={async () => {
-                const result = await Swal.fire({
-                  title: `Delete this text?`,
-                  html: `<span class="text-gray-400">${item.text}</span>`,
-                  // icon: 'warning',
-                  showCancelButton: true,
-                  confirmButtonColor: '#3085d6',
-                  cancelButtonColor: '#d33',
-                  confirmButtonText: 'Yes, delete it!',
-                })
-
-                if (result.isConfirmed) {
-                  await fetch('/api/delete', {
-                    method: 'delete',
-                    body: JSON.stringify({ _id: item._id }),
-                  })
-                  toast.success('deleted')
-                }
-              }}
-            >
-              <IconDelete size={30} />
             </div>
           </div>
         </div>
