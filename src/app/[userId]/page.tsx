4db0739c5b9509517a59aa3a02fefb23ca7791f2
keating <min9nim@gmail.com>
2023-12-17 22:50:35 +0900

    logo size

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -27,7 +27,7 @@ export default function Home({ params }) {
   return (
     <main className="flex min-h-screen flex-col items-center px-2 py-4 gap-4">
       <div className="flex gap-2 items-center w-full max-w-2xl">
-        <IconLogo size={300} />
+        <IconLogo size={30} />
         <div className="text-2xl">Copy & Paste</div>
       </div>
       <Form userId={userId} setList={setList} />
